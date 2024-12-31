19e69751a617d24bc36857608d56ee75a5de95db
RafaelPierotti <rafaelpierotti236@gmail.com>
2024-12-30 23:28:04 -0300

    ID Criado

---
+++ src/test/java/Gerenciador/Pedidos/GerenciadorPedidos/GerenciadorPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package Gerenciador.Pedidos.GerenciadorPedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
